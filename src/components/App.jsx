c19c99a8e299e3660bc3b4c08ebdea234e34a198
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-03-31 11:50:55 -0300

    feat: adiciona App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <div className="body">
+        <div className="page">
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
